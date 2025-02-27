40449bd5b987aed5b55ac56b4a694c4e338c5a01
Bessouat40 <roman.bessouat@orange.fr>
2025-02-27 18:51:06 +0100

    fix minio ingestion

--- src/config/config.py
+++ src/config/config.py
@@ -5,7 +5,7 @@ class Config:
     EMBEDDING_MODEL = os.environ.get("EMBEDDING_MODEL", "all-MiniLM-L6-v2")
     COLLECTION_NAME = os.environ.get("COLLECTION_NAME")
     MINIO_ENDPOINT = os.environ.get("MINIO_ENDPOINT")
-    MINIO_ACCESS_KEY = os.environ.get("MINIO_ACCESS_KEY")
-    MINIO_SECRET_KEY = os.environ.get("MINIO_SECRET_KEY")
+    MINIO_ROOT_USER = os.environ.get("MINIO_ROOT_USER")
+    MINIO_ROOT_PASSWORD = os.environ.get("MINIO_ROOT_PASSWORD")
     BUCKET_NAME = "llm-pdf"
     ARXIV_TOPIC = os.environ.get("ARXIV_TOPIC")
--- src/prefect/data_pipeline.py
+++ src/prefect/data_pipeline.py
@@ -1,12 +1,13 @@
-from prefect import task
+from prefect import task, get_run_logger
 from src.storage.minio_client import MinIOClient
 from src.config import Config
 @task
 def ingest_articles_in_minio(bucket_name: str, articles: list) -> str:
+    logger = get_run_logger()
     minio_client = MinIOClient(
         endpoint=Config.MINIO_ENDPOINT,
-        access_key=Config.MINIO_ACCESS_KEY,
-        secret_key=Config.MINIO_SECRET_KEY
+        access_key=Config.MINIO_ROOT_USER,
+        secret_key=Config.MINIO_ROOT_PASSWORD
     )
     minio_client.ingestDocsInMinio(bucket_name, articles)
     return f"Ingested {len(articles)} articles into bucket '{bucket_name}'"
